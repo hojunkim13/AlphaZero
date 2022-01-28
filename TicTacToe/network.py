cba88e669d698905d874362b9b04ba11b04fb2f0
hojunkim13 <hojunkim13@gmail.com>
2022-01-28 10:43:06 +0900

    Simplify architecture

--- TicTacToe/network.py
+++ TicTacToe/network.py
@@ -4,41 +4,31 @@ from utils import preprocess
 from game import BOARD_SIZE
 
 
-class conv_layer(nn.Module):
-    def __init__(self, dim_in, dim_out, kernel_size, stride, padding):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(
-                dim_in,
-                dim_out,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-                bias=False,
-            ),
-            nn.BatchNorm2d(dim_out),
-            nn.ReLU(),
-        )
-
-    def forward(self, x):
-        return self.net(x)
-
-
 class CNN(nn.Module):
     def __init__(self, hidden_dim=64) -> None:
         super().__init__()
         self.net = nn.Sequential(
-            conv_layer(2, hidden_dim, 3, 1, 1),
-            *[conv_layer(hidden_dim, hidden_dim, 3, 1, 1) for _ in range(3)],
-            nn.Flatten(),
+            nn.Conv2d(2, hidden_dim, 3, 1, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU(),
+            nn.Conv2d(hidden_dim, hidden_dim, 3, 1, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU(),
         )
 
         self.policy_net = nn.Sequential(
-            nn.Linear(hidden_dim * BOARD_SIZE * BOARD_SIZE, BOARD_SIZE * BOARD_SIZE),
+            nn.Conv2d(hidden_dim, 1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(BOARD_SIZE * BOARD_SIZE, BOARD_SIZE * BOARD_SIZE),
             nn.Softmax(-1),
         )
         self.value_net = nn.Sequential(
-            nn.Linear(hidden_dim * BOARD_SIZE * BOARD_SIZE, 1), nn.Tanh()
+            nn.Conv2d(hidden_dim, 1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(BOARD_SIZE * BOARD_SIZE, 1),
+            nn.Tanh(),
         )
 
     def forward(self, x):
